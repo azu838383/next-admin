7ddeacc76b6c24845840e124e957fb6c05b8f36c
Adri Syahrul <azu838383@gmail.com>
2023-12-29 20:56:58 +0800

    OK

--- pages/component/loading.tsx
+++ pages/component/loading.tsx
@@ -1,10 +1,12 @@
 import { useLoading } from "@/components/Loading";
 import Layout from "@/components/layout";
 import CardLayout from "@/components/layout/CardLayout";
-import { Button } from "@mantine/core";
+import { ActionIcon, Button } from "@mantine/core";
 import Head from "next/head";
 import { FaClock } from "react-icons/fa";
 import appConfig from "../../app.json";
+import { useState } from "react";
+import { IconSettings } from "@tabler/icons-react";
 
 export default function LoadingPage() {
 	const {
@@ -21,13 +23,15 @@ export default function LoadingPage() {
 		}, 3000);
 	};
 
+	const [isHover, setIsHover] = useState<number[]>([1])
+
 	return (
 		<>
 			<Head>
 				<title>Notification & Loading | {appConfig.name}</title>
 			</Head>
 			<Layout>
-				<div className="grid grid-cols-2">
+				<div className="grid grid-cols-2 gap-4">
 					<CardLayout>
 						<div className="grid grid-cols-2 gap-4">
 							<Button
@@ -87,6 +91,60 @@ export default function LoadingPage() {
 								Error Catching, can be use on trycatch function
 							</Button>
 						</div>
+					</CardLayout>
+					<CardLayout className="text-center flex flex-col gap-4 items-center">
+						Are you looser?
+						<div className="flex gap-4 relative ">
+							<Button
+							className={`absolute transition-all !w-20 ${
+								isHover.includes(1)?"-left-0 top-0":
+								isHover.includes(2)?"left-0 top-14":
+								isHover.includes(3)?"left-24 top-14":
+								isHover.includes(4)?"left-24 top-0":
+								""
+							}`}
+							onClick={()=>{
+								addNotification({
+									position: "top-right",
+									message: "Yes I know 🤣",
+									type: "success",
+								});
+							}}
+							>
+								Yes
+							</Button>
+							<Button
+							color={"red"}
+							className={`absolute transition-all !w-20 ${
+								isHover.includes(1)?"left-0 top-0":
+								isHover.includes(2)?"-left-24 top-0":
+								isHover.includes(3)?"-left-24 top-14":
+								isHover.includes(4)?"left-0 top-14":
+								""
+							}`}
+							onClick={()=>{
+								addNotification({
+									position: "top-right",
+									message: "You shouldn't be able to click on this 🤣",
+									type: "error",
+								});
+							}}
+							onMouseEnter={()=>{
+								if(isHover.includes(1)){
+									setIsHover([2])
+								} else if(isHover.includes(2)) {
+									setIsHover([3])
+								} else if(isHover.includes(3)) {
+									setIsHover([4])
+								} else {
+									setIsHover([1])
+								}
+							}}
+							>
+								No
+							</Button>
+						</div>
+						
 					</CardLayout>
 				</div>
 			</Layout>
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/layout";
 import CardLayout from "@/components/layout/CardLayout";
 import { GetProductWPagination, GetSingleProduct, IProductItems, SearchProduct } from "@/libs/api/product";
-import { Badge, Button, Card, Group, Modal, NumberFormatter, Pagination, Select, Skeleton, Text, TextInput } from "@mantine/core";
+import { AspectRatio, Badge, Button, Card, Group, Modal, NumberFormatter, Pagination, Select, Skeleton, Text, TextInput } from "@mantine/core";
 import { useDebouncedState } from "@mantine/hooks";
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
@@ -147,6 +147,15 @@ export default function Dashboard() {
 							/>
 						</div>
 					</div>
+				</CardLayout>
+				<CardLayout className="mt-4">
+					<AspectRatio ratio={16 / 9}>
+					<iframe
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d103898.6272928707!2d120.96569110347316!3d14.59019439975459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b795a0e244e5%3A0x6a1a7885c2c5109!2sArt%20In%20Island!5e0!3m2!1sid!2sph!4v1703822616141!5m2!1sid!2sph"
+						title="Google map"
+						frameBorder="0"
+					/>
+					</AspectRatio>
 				</CardLayout>
 				<Modal
 					opened={detailVisible}
